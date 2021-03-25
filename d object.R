c7a5cfd578d1e751dd51a84fc811039b390dc7f6
Amit Kumar Paikra <70443251+AMITPKR@users.noreply.github.com>
2021-03-25 22:10:39 +0530

    Add files via upload
    It is the R code file for example of pie chart.

---
+++ d object.R
@@ -0,0 +1,8 @@
+library("plotrix")            #get the library
+x<-c(10,5,100,4)              #create population data in Lakh for the length 
+lbl<-c("Raipur","Bilaspur","Delhi","Goa") 
+perc<-round(100*x/sum(x),1)       # % for all city
+png(file="2Dpiepercentage.png")        #give the file name
+pie(x,lbl=perc,main="city pie chart based on their population",col=rainbow(length(x))) #plot the chart
+legend("topright",lbl,cex=0.8,fill=rainbow(length(x)))
+dev.off()     #save the file
